41270481c283c3042e468306b2a057351fece587
Quentin Perez <qperez@ocs.online.net>
2015-12-09 17:49:32 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use std::env;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let mut size:i32 = 20;
+    let mut size: i32 = 20;
     let padding = vec!["  ", " ", "", " ", "  ", "   ", "    ", "   "];
 
     if args.len() > 1 {
-        size = args[1].parse()
-                .ok()
-                .expect("Wanted a number");
+        size = args[1]
+                   .parse()
+                   .ok()
+                   .expect("Wanted a number");
     }
     if size >= 0 {
-        let mut i:usize = 0;
+        let mut i: usize = 0;
 
         println!("    ╚⊙ ⊙╝");
         while i < size as usize {
@@ -19,10 +20,10 @@ fn main() {
             i = i + 1;
         }
     } else {
-        let mut i:usize = size.abs() as usize;
+        let mut i: usize = size.abs() as usize;
 
         while i > 0 {
-            println!("{}╔═(███)═╗", padding[i%8]);
+            println!("{}╔═(███)═╗", padding[i % 8]);
             i = i - 1;
         }
         println!("   ╔⊙ ⊙╗");
