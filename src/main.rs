b845d922f1ee1c135921f21821f35764c02dc33b
Quentin Perez <qperez@ocs.online.net>
2015-12-15 18:27:13 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,17 @@ fn main() {
     let mut skin = "███".to_string();
     let padding = vec!["  ", " ", "", " ", "  ", "   ", "    ", "   "];
 
-    {  // this block limits scope of borrows by ap.refer() method
+    {
+        // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
 
         ap.set_description("Print a beautiful millipede (written in rust)");
         ap.refer(&mut size)
-            .add_option(&["-w", "--width"], Store,
-            "millipede width");
+          .add_option(&["-w", "--width"], Store, "millipede width");
         ap.refer(&mut skin)
-            .add_option(&["-s", "--skin"], Store,
-            "millipede skin");
+          .add_option(&["-s", "--skin"], Store, "millipede skin");
         ap.refer(&mut reverse)
-            .add_option(&["-r", "--reverse"], StoreTrue,
-            "reverse the millipede");
+          .add_option(&["-r", "--reverse"], StoreTrue, "reverse the millipede");
         ap.parse_args_or_exit();
     }
     if reverse == false {
