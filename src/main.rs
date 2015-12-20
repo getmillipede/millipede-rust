ae302027db0db31a8d473fa06ec4a27103036510
Quentin Perez <qperez42@gmail.com>
2015-12-20 18:21:13 +0100

    Add http server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 extern crate argparse;
+extern crate iron;
+
 use argparse::{ArgumentParser, StoreTrue, Store};
+use iron::prelude::*;
+
+fn http_server(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "Rust server")))
+}
+
+static HTTP_ADDR: &'static str = "localhost:4242";
 
 fn main() {
     let mut size: usize = 20;
     let mut reverse: bool = false;
+    let mut server_mode: bool = false;
     let mut skin = "███".to_string();
     let padding = vec!["  ", " ", "", " ", "  ", "   ", "    ", "   "];
 
@@ -18,8 +28,17 @@ fn main() {
           .add_option(&["-s", "--skin"], Store, "millipede skin");
         ap.refer(&mut reverse)
           .add_option(&["-r", "--reverse"], StoreTrue, "reverse the millipede");
+        ap.refer(&mut server_mode)
+          .add_option(&["--server"], StoreTrue, "Spawn http server");
         ap.parse_args_or_exit();
     }
+    if server_mode == true {
+        let chain = Chain::new(http_server);
+
+        println!("Listening on {}", HTTP_ADDR);
+        Iron::new(chain).http(HTTP_ADDR).unwrap();
+    }
+
     if reverse == false {
         let mut i: usize = 0;
 
